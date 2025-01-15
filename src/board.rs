432005bd02e8e9c929bd0b87bb460868453634bb
Samman Bajracharya <sammanbajracharya@gmail.com>
2025-01-15 17:32:46 +0545

    Check for wins

--- src/board.rs
+++ src/board.rs
@@ -1,6 +1,6 @@
 use colored::Colorize;
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, PartialEq)]
 pub enum Piece {
     None,
     Red,
@@ -59,8 +59,39 @@ impl Board {
     }
 
     pub fn highlight_col(&mut self, x: usize) {
+        self.highlights = [[false; 6]; 7];
         for y in 0..6 {
             self.highlights[x][y] = true;
         }
+    }
+
+    pub fn check_for_win(&mut self, x: usize, y: usize, piece: Piece) -> bool {
+        let check_bound: [[(isize, isize); 2]; 4] = [
+            [(-1, -1), (1, 1)], // Diagonal top-left to bottom-right
+            [(1, -1), (-1, 1)], // Diagonal bottom-left to top-right
+            [(1, 0), (-1, 0)],  // Horizontal
+            [(0, 1), (0, -1)],  // Vertical
+        ];
+
+        for &directions in &check_bound {
+            let mut count = 1;
+
+            for &(dx, dy) in directions.iter() {
+                let mut curr_x = x as isize;
+                let mut curr_y = y as isize;
+
+                if !(0..7).contains(&curr_x) || !(0..6).contains(&curr_y) { break; }
+
+                if self.board[curr_x as usize][curr_y as usize] == piece { count += 1; }
+                else { break; }
+
+                curr_x += curr_x.saturating_add(dx);
+                curr_y += curr_y.saturating_add(dy);
+            }
+
+            if count >= 4 { return true; }
+        }
+
+        false
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,9 @@ use board::{Board, Piece};
 
 fn main() {
     let mut board = Board::new();
-    board.print();
-
     board.drop_piece(3, Piece::Blue);
-    board.drop_piece(3, Piece::Red);
+    board.drop_piece(3, Piece::Blue);
+    board.drop_piece(3, Piece::Blue);
     board.drop_piece(3, Piece::Blue);
     board.highlight_col(3);
     board.print();
